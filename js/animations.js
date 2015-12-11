49790b04d1be1345d9c2d3a038ea3413bfc996c5
ceckenrode <chriseckenrode@me.com>
2015-12-10 23:23:22 -0500

    added some new fonts, added shake animation, started styling gameboy

--- js/animations.js
+++ js/animations.js
@@ -2,17 +2,25 @@ $(document).ready(function() {
 
 
     $( "#clickme" ).click(function() {
-  $( "#userSelection" ).animate({
-    marginTop: "60px",
-    opacity: 0.0,
-  }, 300 );
+      $( "#userSelection" ).effect("shake", {
+      times:10
+    }, 800)
+  //$( "#userSelection" ).animate({
+    //marginTop: "60px",
+    //opacity: 0.0,
+  //}, 300 );
 });
 
   $( "#dontclickme" ).click(function() {
-  $( "#compSelection" ).animate({
-    marginTop: "60px",
-    opacity: 0.0,
-  }, 300 );
+    $( "#compSelection" ).effect("shake", {
+      times:10
+    }, 800)
+
+    //.animate({
+
+    //marginTop: "60px",
+    //opacity: 0.0,
+  //}, 300 );
 });
 
   
